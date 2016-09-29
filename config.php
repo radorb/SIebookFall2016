7fe4c4371c847cbb6a03d9dc9d0d89575a0baf8d
Roger <Roger@System4>
2016-09-29 11:54:15 -0400

    new commit

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
    define('DB_SERVER', 'localhost:3306');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'SIebookFall2016');
-   define('DB_DATABASE', 'ebook');
+   define('DB_DATABASE', 'MySQL57');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
