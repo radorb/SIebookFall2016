97bff19c1d1f26ef084a00410601330fba3c45cd
Roger <Roger@System4>
2016-09-29 14:59:04 -0400

    new commit

--- welcome.php
+++ welcome.php
@@ -9,11 +9,9 @@
    
    <body>
       <h1>Welcome, <?php echo $login_session; ?>.</h1>
-      <br>
       <p><a href="index.php">Edit Chapters</a></p>
       <p><a href="index.php">Edit Sections</a></p>
       <p><a href="index.php">Edit Links</a></p>
-      <br>
       <h2><a href = "logout.php">Logout</a></h2>
    </body>
    
