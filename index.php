caa32fe87d6b3ac6ab7d62b088b188c6e2e71802
Roger <Roger@System4>
2016-09-29 12:41:07 -0400

    new commit

--- index.php
+++ index.php
@@ -1,17 +1,123 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+   
+?>
+
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Technology Boot Camp - Home</title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<head>
+  <title>Textbook Title</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+  body {
+      position: relative;
+  }
+  #chapter1 {padding-top:50px;height:500px;border-bottom: thin solid #000000;}
+  #chapter2 {padding-top:50px;height:500px;border-bottom: thin solid #000000;}
+  #chapter3 {padding-top:50px;height:500px;border-bottom: thin solid #000000;}
+  #chapter41 {padding-top:50px;height:500px;border-bottom: thin solid #000000;}
+  #chapter42 {padding-top:50px;height:500px;border-bottom: thin solid #000000;}
+  </style>
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Textbook Title</a>
+    </div>
+    <div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li><a href="#chapter1">Chapter 1</a></li>
+          <li><a href="#chapter2">Chapter 2</a></li>
+          <li><a href="#chapter3">Chapter 3</a></li>
+          <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Chapter 4 <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#chapter41">Chapter 4-1</a></li>
+              <li><a href="#chapter42">Chapter 4-2</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<div id="chapter1" class="container-fluid">
+  <h1>Chapter 1</h1>
+  <p><a href="index.html">Chapter 1 Link 1</a></p>
+  <p><a href="index.html">Chapter 1 Link 2</a></p>
+  <p><a href="index.html">Chapter 1 Link 3</a></p>
+  <p><a href="index.html">Chapter 1 Link 4</a></p>
+  <p><a href="index.html">Chapter 1 Link 5</a></p>
+  <p><a href="index.html">Chapter 1 Link 6</a></p>
+  <p><a href="index.html">Chapter 1 Link 7</a></p>
+  <p><a href="index.html">Chapter 1 Link 8</a></p>
+  <p><a href="index.html">Chapter 1 Link 9</a></p>
+  <p><a href="index.html">Chapter 1 Link 10</a></p>
+  
+</div>
+<div id="chapter2" class="container-fluid">
+  <h1>Chapter 2</h1>
+  <p><a href="index.html">Chapter 2 Link 1</a></p>
+  <p><a href="index.html">Chapter 2 Link 2</a></p>
+  <p><a href="index.html">Chapter 2 Link 3</a></p>
+  <p><a href="index.html">Chapter 2 Link 4</a></p>
+  <p><a href="index.html">Chapter 2 Link 5</a></p>
+  <p><a href="index.html">Chapter 2 Link 6</a></p>
+  <p><a href="index.html">Chapter 2 Link 7</a></p>
+  <p><a href="index.html">Chapter 2 Link 8</a></p>
+  <p><a href="index.html">Chapter 2 Link 9</a></p>
+  <p><a href="index.html">Chapter 2 Link 10</a></p>
+  
+</div>
+<div id="chapter3" class="container-fluid">
+  <h1>Chapter 3</h1>
+  <p><a href="index.html">Chapter 3 Link 1</a></p>
+  <p><a href="index.html">Chapter 3 Link 2</a></p>
+  <p><a href="index.html">Chapter 3 Link 3</a></p>
+  <p><a href="index.html">Chapter 3 Link 4</a></p>
+  <p><a href="index.html">Chapter 3 Link 5</a></p>
+  <p><a href="index.html">Chapter 3 Link 6</a></p>
+  <p><a href="index.html">Chapter 3 Link 7</a></p>
+  <p><a href="index.html">Chapter 3 Link 8</a></p>
+  <p><a href="index.html">Chapter 3 Link 9</a></p>
+  <p><a href="index.html">Chapter 3 Link 10</a></p>
+</div>
+<div id="chapter41" class="container-fluid">
+  <h1>Chapter 4 Section 1</h1>
+  <p><a href="index.html">Chapter 4 Section 1 Link 1</a></p>
+  <p><a href="index.html">Chapter 4 Section 1 Link 2</a></p>
+  <p><a href="index.html">Chapter 4 Section 1 Link 3</a></p>
+  <p><a href="index.html">Chapter 4 Section 1 Link 4</a></p>
+  <p><a href="index.html">Chapter 4 Section 1 Link 5</a></p>
+  <p><a href="index.html">Chapter 4 Section 1 Link 6</a></p>
+  <p><a href="index.html">Chapter 4 Section 1 Link 7</a></p>
+  <p><a href="index.html">Chapter 4 Section 1 Link 8</a></p>
+  <p><a href="index.html">Chapter 4 Section 1 Link 9</a></p>
+  <p><a href="index.html">Chapter 4 Section 1 Link 10</a></p>
+</div>
+<div id="chapter42" class="container-fluid">
+  <h1>Chapter 4 Section 2</h1>
+  <p><a href="index.html">Chapter 4 Section 2 Link 1</a></p>
+  <p><a href="index.html">Chapter 4 Section 2 Link 2</a></p>
+  <p><a href="index.html">Chapter 4 Section 2 Link 3</a></p>
+  <p><a href="index.html">Chapter 4 Section 2 Link 4</a></p>
+  <p><a href="index.html">Chapter 4 Section 2 Link 5</a></p>
+  <p><a href="index.html">Chapter 4 Section 2 Link 6</a></p>
+  <p><a href="index.html">Chapter 4 Section 2 Link 7</a></p>
+  <p><a href="index.html">Chapter 4 Section 2 Link 8</a></p>
+  <p><a href="index.html">Chapter 4 Section 2 Link 9</a></p>
+  <p><a href="index.html">Chapter 4 Section 2 Link 10</a></p>
+</div>
+
+</body>
 </html>
